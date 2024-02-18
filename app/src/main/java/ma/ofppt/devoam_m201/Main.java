ecb019efa55d608e2146d7602d9600a81bb29235
Omar OUGHZAL <Omar.oughzal@gmail.com>
2024-02-18 03:31:35 +0100

    Initial Commit

---
+++ app/src/main/java/ma/ofppt/devoam_m201/Main.java
@@ -0,0 +1,7 @@
+package ma.ofppt.devoam_m201;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "DEVOAM_M201"
+include(":app")
+ 
