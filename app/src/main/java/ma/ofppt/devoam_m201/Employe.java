0d12c2ef05186908dc461def9f4ffe1cc92e8cda
Omar OUGHZAL <Omar.oughzal@gmail.com>
2024-02-18 03:38:53 +0100

    Initial Commit

---
+++ app/src/main/java/ma/ofppt/devoam_m201/Employe.java
@@ -0,0 +1,60 @@
+package ma.ofppt.devoam_m201;
+
+import java.util.Date;
+
+public abstract class Employe {
+    private int matricule;
+    private String nom, prenom;
+    private Date dateNaissance;
+
+    public Employe(int matricule, String nom, String prenom, Date dateNaissance) {
+        this.matricule = matricule;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.dateNaissance = dateNaissance;
+    }
+
+    public int getMatricule() {
+        return matricule;
+    }
+
+    public void setMatricule(int matricule) {
+        this.matricule = matricule;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public Date getDateNaissance() {
+        return dateNaissance;
+    }
+
+    public void setDateNaissance(Date dateNaissance) {
+        this.dateNaissance = dateNaissance;
+    }
+
+    @Override
+    public String toString() {
+        return "Employe{" +
+                "matricule=" + matricule +
+                ", nom='" + nom + '\'' +
+                ", prenom='" + prenom + '\'' +
+                ", dateNaissance=" + dateNaissance +
+                '}';
+    }
+
+    public abstract double getSalaire();
+}
